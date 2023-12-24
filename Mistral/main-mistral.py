11d1ecd2cc2d1522a6601a2b2e0c1e4824d8013c
Aziz Chikh <azizchpay99@outlook.com>
2023-12-24 17:06:34 +0100

    Run Mistral 7B Model for Text completion

---
+++ Mistral/main-mistral.py
@@ -0,0 +1,39 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import transformers
+print(f"Transformers: {transformers.__version__}")
+
+#model_id = "mistralai/Mixtral-8x7B-v0.1"
+
+model_id = "mistralai/mistral-7B-v0.1"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+model = AutoModelForCausalLM.from_pretrained(model_id)
+
+text = "Hello my name is"
+inputs = tokenizer(text, return_tensors="pt")
+
+outputs = model.generate(**inputs, max_new_tokens=20)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+# from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# device = "cuda" # the device to load the model onto
+
+# model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
+# tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
+
+# messages = [
+#     {"role": "user", "content": "What is your favourite condiment?"},
+#     {"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
+#     {"role": "user", "content": "Do you have mayonnaise recipes?"}
+# ]
+
+# encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
+
+# model_inputs = encodeds.to(device)
+# model.to(device)
+
+# generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
+# decoded = tokenizer.batch_decode(generated_ids)
+# print(decoded[0])
+
